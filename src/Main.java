5aaf6a75649ac879184b8b3b09f17bb89e0f425b
Sarah Azka Arief <sarah_azka@yahoo.com>
2021-09-26 22:30:31 +0700

    add main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 package src;
-import javax.swing.JOptionPane;
+import src.Functions.Interface;
 
 public class Main {
-  // ges ini cuma testing maaf :V
-    public static void main(String[] args) {
-      String input_filename = JOptionPane.showInputDialog(null, "Enter filename:");
-      Matrix m = Func.readMatrix(input_filename);
-      System.out.println(m.cols);
-      System.out.println(m.rows);
-      Func.displayMatrix(m);
-      String output_filename = JOptionPane.showInputDialog(null, "Saving this file as ______.txt (pls fill in da blankz):");
-      Func.writeMatrix(output_filename, m);
+  // masih testing aja ges :V
+  public static void main(String[] args) {
+    while(true){
+      Interface.user();
     }
+  }
 }
