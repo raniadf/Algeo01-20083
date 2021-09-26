e0528a406ff15043ab47060a5d3069060c836b6a
raniadf <16520006@mahasiswa.itb.ac.id>
2021-09-26 23:27:38 +0700

    Change to matrix

--- src/Functions/SPL.java
+++ src/Functions/SPL.java
@@ -1,21 +1,17 @@
 package src.Functions;
 
 public class SPL {
-    public double[][] Gauss(double A[][]) {
-        // Count the rows & columns
-        int RowCount = A.length;
-        int ColCount = A[0].length;
-
+    public static Matrix Gauss(Matrix A) {
         // Set index
         int i, j;
 
         // Forward loop
-        for (i = 0; i < RowCount; ++i) {
+        for (i = 0; i < A.rows; ++i) {
 
-            for (j = 0; j < ColCount; ++j) {
+            for (j = 0; j < A.cols; ++j) {
 
                 // Case matrix = 0
-                if (A[i][j] == 0) {
+                if (Func.getElmt(A, i, j) == 0) {
 
                     // Parameter notzero
                     boolean notZero = false;
@@ -23,29 +19,29 @@ public class SPL {
                     int ColCheck = i + 1;
 
                     // To check the existence of non zero number in the same column
-                    while (ColCheck < RowCount && !notZero) {
-                        if (A[ColCheck][j] != 0) {
+                    while (ColCheck < A.rows && !notZero) {
+                        if (Func.getElmt(A, ColCheck, j) != 0) {
                             notZero = true;
                             // Swap the rows
-                            switchOBE(A, ColCheck + 1, i);
+                            Func.switchOBE(A, ColCheck + 1, i);
                         }
                         ++ColCheck;
                     }
                 }
 
                 // Case matrix != 0
-                if (A[i][j] != 0) {
+                if (Func.getElmt(A, i, j) != 0) {
 
                     // Divide the column by itself so that the leading column = 1
-                    double Divider = A[i][j];
-                    divideOBE(A, i + 1, Divider);
+                    double Divider = Func.getElmt(A, i, j);
+                    Func.divideOBE(A, i + 1, Divider);
 
                     // Substract multiples of the row to other rows
                     double Factor;
                     int OtherRows = i + 1;
-                    while (OtherRows < RowCount) {
-                        Factor = A[OtherRows][j];
-                        addOBE(A, OtherRows + 1, i + 1, ((-1) * Factor));
+                    while (OtherRows < A.rows) {
+                        Factor = Func.getElmt(A, OtherRows, j);
+                        Func.addOBE(A, OtherRows + 1, i + 1, ((-1) * Factor));
                         ++OtherRows;
                     }
 
@@ -56,11 +52,7 @@ public class SPL {
         return A;
     }
 
-    public double[][] GaussJordan(double A[][]) {
-        // Count the rows & columns
-        int RowCount = A.length;
-        int ColCount = A[0].length;
-
+    public static Matrix GaussJordan(Matrix A) {
         // Set index
         int i, j;
 
@@ -68,18 +60,18 @@ public class SPL {
         A = Gauss(A);
 
         // Backward loop
-        for (i = RowCount - 1; i >= 0; --i) {
+        for (i = A.rows - 1; i >= 0; --i) {
 
-            for (j = ColCount - 1; j >= 0; --j) {
+            for (j = A.cols - 1; j >= 0; --j) {
 
                 // Case matrix = 1
-                if (A[i][j] == 1) {
+                if (Func.getElmt(A, i, j) == 1) {
                     // Substract multiples of the row to other rows
                     double Factor;
                     int OtherRows = i - 1;
                     while (OtherRows >= 0) {
-                        Factor = A[OtherRows][j];
-                        addOBE(A, OtherRows + 1, i + 1, ((-1) * Factor));
+                        Factor = Func.getElmt(A, OtherRows, j);
+                        Func.addOBE(A, OtherRows + 1, i + 1, ((-1) * Factor));
                         --OtherRows;
                     }
                 }
