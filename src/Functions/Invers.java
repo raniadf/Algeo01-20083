7196148de438d5a4f501a2f81aec1f7843093b37
Nelsen Putra <16520272@std.stei.itb.ac.id>
2021-09-28 23:53:54 +0700

    move adjoin function and gaussJordan function to Invers.java

--- src/Functions/Invers.java
+++ src/Functions/Invers.java
@@ -38,4 +38,57 @@ public class Invers {
         }
         return m;
     }
+
+    /** ADJOIN
+     * Mengembalikan inverse dari matriks dengan metode kofaktor/adjoin
+     * Prekondisi: matriks persegi (isSquare(m))
+     * @param m matriks yang ingin dicari inversnya
+     * @return matriks invers dari m
+     */
+    public static Matrix adjoin(Matrix m) {
+        Matrix adjointMat = new Matrix(m.rows, m.cols);
+        Matrix inverseMat = new Matrix(m.rows, m.cols);
+
+        double det = Determinant.cofExp(m);
+        adjointMat = Func.transpose(Func.makeCofactor(m));
+
+        int i, j;
+        for (i = 0; i < m.rows; i++) {
+            for (j = 0; j < m.cols; j++) {
+                inverseMat.contents[i][j] = (1 / det) * adjointMat.contents[i][j];
+            }
+        }
+        return inverseMat;
+    }
+
+    /** ADJOIN
+     * Mengembalikan inverse dari matriks dengan metode Gauss Jordan
+     * Prekondisi: matriks persegi (isSquare(m))
+     * @param m matriks yang ingin dicari inversnya
+     * @return matriks invers dari m
+     */
+    public static Matrix gaussJordan(Matrix m) {
+        Matrix inverseMat = new Matrix(m.rows, m.cols);
+        Matrix temp = new Matrix(m.rows, m.cols * 2);
+
+        int i, j;
+        for (i = 0; i < m.rows; i++) {
+            for (j = 0; j < m.cols; j++) {
+                temp.contents[i][j] = m.contents[i][j];
+            }
+        }
+
+        for (i = 0; i < m.rows; i++) {
+            temp.contents[i][i + m.cols] = 1;
+        }
+
+        int i2, j2;
+        SPL.GaussJordan(m); 
+        for (i2 = 0; i2 < m.rows; i2++) {
+            for (j2 = 0; j2 < m.cols; j2++) {
+                inverseMat.contents[i2][j2] = temp.contents[i2][j2 + m.cols];
+            }
+        }
+        return inverseMat;
+    }
 }
