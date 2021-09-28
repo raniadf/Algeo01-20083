d887f519b88bc650761802524fce42ce23df2285
Sarah Azka Arief <sarah_azka@yahoo.com>
2021-09-29 02:25:23 +0700

    add finished cramer function

--- src/Functions/Cramer.java
+++ src/Functions/Cramer.java
@@ -1,9 +1,8 @@
 package src.Functions;
 import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
 
 public class Cramer {
-    // belom bisa dipakekkk
-    // tinggal fill in the comments aja... kalo gaada yg salah XD LOL bau2nya ada si LOL XD
     public static Matrix solveSPL(Matrix m){
         if (m.rows != m.cols - 1){
             JOptionPane.showMessageDialog(null, "Dibutuhkan " + (m.cols - 1) + " buah persamaan untuk " + (m.cols - 1) + " buah variabel", "Try again", JOptionPane.WARNING_MESSAGE);
@@ -12,7 +11,6 @@ public class Cramer {
         }
         Matrix A = new Matrix(m.rows, m.cols - 1);
         Matrix b = new Matrix(m.rows, 1);
-        Matrix temp = new Matrix(m.rows, m.cols - 1);
 
         int i, j;
         for (i = 0; i <= Func.getLastIdxRow(A); i++){
@@ -24,14 +22,23 @@ public class Cramer {
                 Func.setElmt(b, i, 0, Func.getElmt(m, i, Func.getLastIdxCol(m)));
         }
 
-        // detA = Determinant(A) cari determinan A dulu
+        double detA = Determinant.cofExp(A);
+        if (detA == 0){
+            JOptionPane.showMessageDialog(null, "Determinan = 0!\nSistem tidak bisa diselesaikan dengan metode ini!", "Uh oh...", JOptionPane.WARNING_MESSAGE);
+            Interface.user();
+        }
+        System.out.println("\n");
+        System.out.println(detA);
+        System.out.println("\n");
+        Matrix temp;
         for (j = 0; j <= Func.getLastIdxCol(A); j++){
-            temp = A;
+            temp = Func.copyMatrix(A);
             for (i = 0; i <= Func.getLastIdxRow(A); i++){
                 Func.setElmt(temp, i, j, Func.getElmt(b, i, 0));
             }
-            // detTemp = Determinant(temp) cari determinannya
-            //System.out.print("x" + (j+1) + " = " + (detTemp/detA) + "\n");
+            double detTemp = Determinant.cofExp(temp);
+            DecimalFormat df = new DecimalFormat("####0.00");
+            System.out.print("x" + (j+1) + " = " + df.format((detTemp/detA)) + "\n");
         }
         return m;
     }
