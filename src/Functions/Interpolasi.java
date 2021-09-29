c3f2489c97e5782a5e6191f69b541057c6758283
Sarah Azka Arief <sarah_azka@yahoo.com>
2021-09-29 19:13:45 +0700

    refine finished cramer function

--- src/Functions/Interpolasi.java
+++ src/Functions/Interpolasi.java
@@ -3,37 +3,49 @@ import java.text.DecimalFormat;
 import java.lang.Math;
 
 public class Interpolasi {
-    // belom bisa dipakekkk DAN MASIH KASAR BAGNETNGETNGET
-    // tinggal fill in the comments aja... kalo gaada yg salah XD LOL bau2nya ada si LOL XD
-    public static double solveInterpolasi(Matrix m, double x){
-        Matrix p2x = new Matrix(m.rows, 4);
-        double tot = 0; // INI GANTI!!!!
-        int i, j;
 
-        for (i = 0; i < p2x.rows; i++){
-            for (j = 0; j < p2x.cols; j++){
+    /** SOLVE INTERPOLASI
+     * Menaksir nilai y di sembarang titik dengan menentukan polinom pn(x) dari n+1 buah titik
+     * @param m matriks yang berisi titik-titik
+     * @param x titik yang nilai fungsinya akan ditaksir
+     * @return nilai y di titik x
+     */
+    public static double solveInterpolasi(Matrix m, double x){
+        Matrix px = new Matrix(m.rows, m.rows + 1);
+        double tot = 0;
+        int i, j;
+        // Membuat matrix baru berisi sistem persamaan lanjar
+        for (i = 0; i <= Func.getLastIdxRow(px); i++){
+            for (j = 0; j <= Func.getLastIdxCol(px); j++){
                 if (j == 0){
-                    Func.setElmt(p2x, i, j, 1);
+                    Func.setElmt(px, i, j, 1);
                 }
-                else if (j == 1){
-                    Func.setElmt(p2x, i, j, Func.getElmt(m, i, 0));
-                }
-                else if (j == 2){
-                    Func.setElmt(p2x, i, j, Math.pow(Func.getElmt(m, i, 0), 2));
+                else if (j == Func.getLastIdxCol(px)){
+                    Func.setElmt(px, i, j, Func.getElmt(m, i, 1));
                 }
                 else{
-                    Func.setElmt(p2x, i, j, Func.getElmt(m, i, 1));
+                    Func.setElmt(px, i, j, Math.pow(Func.getElmt(m, i, 0), j));
                 }
             }
         }
-        // lakuin solveSPLGauss ke matrixnya
-        // hasil SPLGaussnya disimpen di array double
-        p2x = SPL.Gauss(p2x);
-        double[] result = SPL.UniqueSPL(p2x);
+        // Menyelesaikan sistem persamaan dengan metode eliminasi Gauss
+        px = SPL.Gauss(px);
+        double[] result = SPL.UniqueSPL(px);
+        DecimalFormat df = new DecimalFormat("####0.0000");
+        System.out.print("p" + Func.getLastIdxRow(m) + "(x) = ");
         for (i = 0; i < result.length - 1; i++){
+            if (i == 0){
+                System.out.print(df.format(result[i]) + " + ");
+            }
+            else if (i == result.length - 2){
+                System.out.print(df.format(result[i]) + " x^" + i);
+            }
+            else{
+                System.out.print(df.format(result[i]) + " x^" + i + " + ");
+            }
             tot += result[i] * Math.pow(x, i);
         }
-        DecimalFormat df = new DecimalFormat("####0.00");
+        System.out.println("\n");
         System.out.println("Hasil penaksiran: " + df.format(tot));
         return tot;
     }
